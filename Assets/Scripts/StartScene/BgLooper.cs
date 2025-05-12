f30f3f4063d40ebdc19e049e2918727a70ccd051
laflame7387 <laflame7387@gmail.com>
2025-05-12 11:48:10 +0900

    del : 이지 차라 폴더
    외부에 있더 에셋폴더 삭제

--- Assets/Scripts/StartScene/BgLooper.cs
+++ Assets/Scripts/StartScene/BgLooper.cs
@@ -4,8 +4,5 @@ using UnityEngine;
 
 public class BgLooper : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        gameObject.GetComponent<Collider2D>
-    }
+   
 }
