7124a9e908fdfe0c77c3845027b7d5245fe4d8db
laflame7387 <laflame7387@gmail.com>
2025-05-12 10:57:15 +0900

    feat :  인트로 배경 반복

--- Assets/Scripts/StartScene/BgLooper.cs
+++ Assets/Scripts/StartScene/BgLooper.cs
@@ -4,5 +4,8 @@ using UnityEngine;
 
 public class BgLooper : MonoBehaviour
 {
-    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        gameObject.GetComponent<Collider2D>
+    }
 }
