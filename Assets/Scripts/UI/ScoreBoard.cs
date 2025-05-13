1a587775e2ec1606d3118a507ccc7d33584ed9c1
hkY333 <kimabc0630@naver.com>
2025-05-13 17:59:58 +0900

    text : ScoreBoard 약 수정

--- Assets/Scripts/UI/ScoreBoard.cs
+++ Assets/Scripts/UI/ScoreBoard.cs
@@ -6,8 +6,8 @@ public class ScoreBoard : MonoBehaviour
 {
     private ScoreManager scoreManager;
 
-    [SerializeField] private int nowScore;
-    public int NowScore => nowScore;
+    //[SerializeField] private int nowScore;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +18,6 @@ public class ScoreBoard : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //ScoreManager.Instance.
     }
 }
