02ef52699030f96f4ddef9df5b09b0366fa24c6a
hkY333 <kimabc0630@naver.com>
2025-05-13 12:44:49 +0900

    feat : PlayerController
    Box -> Capsule

--- Assets/Scripts/GameScene/PlayerController.cs
+++ Assets/Scripts/GameScene/PlayerController.cs
@@ -24,7 +24,7 @@ public class PlayerController : MonoBehaviour
     private bool IsDamaged = false; // 데미지 상태 확인 변수
     private bool IsDie = false; // 사망 상태 확인 변수
     private bool IsCrouching = false; // 크라우치 상태 확인 변수
-    private BoxCollider2D boxCollider; // BoxCollider2D 컴포넌트
+    private CapsuleCollider2D capsuleCollider; // BoxCollider2D 컴포넌트
     private Vector2 originalColliderSize; // 원래 콜라이더 크기
     private Vector2 crouchingColliderSize; // 크라우칭 시 콜라이더 크기
 
@@ -35,7 +35,7 @@ public class PlayerController : MonoBehaviour
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        boxCollider = GetComponent<BoxCollider2D>(); // BoxCollider2D 초기화
+        capsuleCollider = GetComponent<CapsuleCollider2D>(); // BoxCollider2D 초기화
         StartCoroutine(ReduceHealthOverTime());
 
         if (rb == null)
@@ -45,13 +45,13 @@ public class PlayerController : MonoBehaviour
             Debug.LogError("Animator is not assigned!");
     
 
-        if (boxCollider == null)
+        if (capsuleCollider == null)
         {
             Debug.LogError("BoxCollider2D is not attached to the player!");
         }
         else
         {
-            originalColliderSize = boxCollider.size;
+            originalColliderSize = capsuleCollider.size;
             crouchingColliderSize = new Vector2(originalColliderSize.x, originalColliderSize.y * 0.5f); // y축 크기를 절반으로 줄임
         }
     }
@@ -98,9 +98,9 @@ public class PlayerController : MonoBehaviour
 
     private void AdjustColliderSize(bool isCrouching)
     {
-        if (boxCollider != null)
+        if (capsuleCollider != null)
         {
-            boxCollider.size = isCrouching ? crouchingColliderSize : originalColliderSize;
+            capsuleCollider.size = isCrouching ? crouchingColliderSize : originalColliderSize;
         }
     }
 
