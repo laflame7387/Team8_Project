4c0001df9bc97cb61ba9ff32975f9b9a6af5174e
laflame7387 <laflame7387@gmail.com>
2025-05-15 03:00:40 +0900

    feat : 게임 이동 기능
    스크립트 구성 및 UI 캔버스 추가

--- Assets/Scripts/MainScene/WarpZone.cs
+++ Assets/Scripts/MainScene/WarpZone.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class WarpZone : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public bool WarpZoneEnter;
+    public GameObject PressKeyUI;
+    public GameObject WarpUI;
+    private void OnTriggerEnter2D(Collider2D collision)
     {
+        if(collision.CompareTag("Player"))
+        {
+            WarpZoneEnter = true;
+            PressKeyUI.SetActive(true);
+            if(Input.GetKey(KeyCode.F))
+            {
+                WarpUI.SetActive(true);
+            }
+        }
         
     }
-
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerExit2D(Collider2D collision)
     {
-        
+        if(collision.CompareTag("Player"))
+        {
+            WarpZoneEnter = false;
+            PressKeyUI.SetActive(false);
+            WarpUI.SetActive(false);
+        }
     }
+    
 }
